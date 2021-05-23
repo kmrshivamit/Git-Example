01e9db31a4f6b552ed67ba1f2705b0d1399cbdc5
M1064451 <M1064451@MT-PG02AL2E>
2021-05-23 23:43:53 +0530

    Done

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,11 @@
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("Checking for mrge Conflict");
 		System.out.println("Lets merge both");
 		System.out.println("New lin added");
-		
-	
+
 	}
 
 }
